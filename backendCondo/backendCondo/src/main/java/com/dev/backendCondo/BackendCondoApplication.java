af539bb235aae15c34276cc61f8b78ea1bf4aade
Didas Morera(Saifodine Carlos) <123031138+DidasMorera@users.noreply.github.com>
2023-11-11 22:40:13 +0200

    iniciando

---
+++ backendCondo/backendCondo/src/main/java/com/dev/backendCondo/BackendCondoApplication.java
@@ -0,0 +1,13 @@
+package com.dev.backendCondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCondoApplication.class, args);
+	}
+
+}
---
+++ backendCondo/backendCondo/src/test/java/com/dev/backendCondo/BackendCondoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.backendCondo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCondoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
